0d117648e4335aaa52949a5b740aaf60c39dbd11
levinh <lethevinh.qng@gmail.com>
2019-08-19 22:09:34 +0700

    s

--- resources/views/admin/international.blade.php
+++ resources/views/admin/international.blade.php
@@ -90,7 +90,61 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Admin</h1>
+                    <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Contestants Information Form
+                            </h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Country</th>
+                                            <th>Name</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Country</th>
+                                            <th>Name</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                    @forelse ($data as $item)
+                                        <tr>
+                                            <td>{{$item->cif_country}}</td>
+                                            <td>{{$item->cif_family_name}}</td>
+                                            <td>Edinburgh</td>
+                                            <td>61</td>
+                                            <td>2011/04/25</td>
+                                            <td>
+                                                <a href="#" class="btn btn-warning btn-circle btn-sm">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                                <a href="#" class="btn btn-danger btn-circle btn-sm">
+                                                    <i class="fas fa-eye"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <p>No data</p>
+                                    @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <!-- /.container-fluid -->
             </div>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/contestants-information-form.html', function () {
+    $data = ContestantsInformationForm::all();
+    return view('admin.information', ["data"=>$data]);
+});
+Route::get('/miss-charm-international-2020.html', function () {
+    $data = ContestantsInformationForm::all();
+    return view('admin.international', ["data"=>$data]);
+});
+Route::get('/official-entry-form.html', function () {
+    $data = ContestantsInformationForm::all();
+    return view('admin.entry', ["data"=>$data]);
+});
+
 Route::get('/admin', function () {
     $data = ContestantsInformationForm::all();
     return view('admin.admin', ["data"=>$data]);
