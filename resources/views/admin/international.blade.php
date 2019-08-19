ea7238c6b3ff2a5752d16ce5e2fc7d781ba42bdf
levinh <lethevinh.qng@gmail.com>
2019-08-19 22:23:03 +0700

    s

--- resources/views/admin/international.blade.php
+++ resources/views/admin/international.blade.php
@@ -90,13 +90,10 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+                    <h1 class="h3 mb-2 text-gray-800">Miss Charm International 2020</h1>
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Contestants Information Form
-                            </h6>
-                        </div>
+
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
