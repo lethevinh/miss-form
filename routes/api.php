bb4883dda2b0fac34acaaa90cc818862fb444736
levinh <lethevinh.qng@gmail.com>
2019-08-21 01:16:46 +0700

    s

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,8 @@ Route::get('miss/contestants-information-form/print/{id}', function (Request $re
         header('Content-Transfer-Encoding: binary');
         header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
         header('Expires: 0');
-        $templateProcessor->save("php://output");
+        $xmlWriter = \PhpOffice\PhpWord\IOFactory::createWriter($templateProcessor, 'Word2007');
+        $xmlWriter->save("php://output");
 
        //  $name = 'app/public/'.Str::slug($mode->cif_country.'-'.$mode->cif_first_name,'-').'.docx';
        // $templateProcessor->saveAs(storage_path($name));
