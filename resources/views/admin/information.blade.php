43a22e69421dc04a8005beae775b8cbd181a6e05
levinh <lethevinh.qng@gmail.com>
2019-08-20 23:51:57 +0700

    s

--- resources/views/admin/information.blade.php
+++ resources/views/admin/information.blade.php
@@ -124,13 +124,41 @@
                                                 @endif
                                             @endforeach
                                             <td>
-                                                <a href="#" class="btn btn-warning btn-circle btn-sm">
+                                                <a href="#" data-toggle="modal" data-target="#exampleModal{{$item->id}}" class="btn btn-warning btn-circle btn-sm">
                                                     <i class="fas fa-edit"></i>
                                                 </a>
-                                                <a href="#" class="btn btn-danger btn-circle btn-sm">
+                                                <a href="#"  class="btn btn-danger btn-circle btn-sm">
                                                     <i class="fas fa-eye"></i>
                                                 </a>
                                             </td>
+                                            <div class="modal fade" id="exampleModal{{$item->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog" role="document">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                    <span aria-hidden="true">&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <form>
+                                                                    <div class="form-group">
+                                                                        <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                                                        <input type="text" class="form-control" id="recipient-name">
+                                                                    </div>
+                                                                    <div class="form-group">
+                                                                        <label for="message-text" class="col-form-label">Message:</label>
+                                                                        <textarea class="form-control" id="message-text"></textarea>
+                                                                    </div>
+                                                                </form>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                                <button type="button" class="btn btn-primary">Send message</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                         </tr>
                                     @empty
                                         <p>No data</p>
