2f3491e940e6a67881a6349074c8d132a951abab
levinh <lethevinh.qng@gmail.com>
2019-10-01 23:29:20 +0700

    s

--- routes/web.php
+++ routes/web.php
@@ -9,28 +9,28 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 use App\ContestantsInformationForm;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Route::get('/contestants-information-form.html', function () {
-    $data = ContestantsInformationForm::all();
-    return view('admin.information', ["data"=>$data, 'fields'=>ContestantsInformationForm::$fields]);
+	$data = ContestantsInformationForm::all();
+	return view('admin.information', ["data" => $data, 'fields' => ContestantsInformationForm::$fields]);
 });
 Route::get('/miss-charm-international-2020.html', function () {
-    $data = \App\NationalDirectorForm::all();
-    return view('admin.international', ["data"=>$data,"fields" => \App\NationalDirectorForm::$fields]);
+	$data = \App\NationalDirectorForm::all();
+	return view('admin.international', ["data" => $data, "fields" => \App\NationalDirectorForm::$fields]);
 });
 Route::get('/official-entry-form.html', function () {
-    $data = \App\OfficialEntryForm::all();
-    return view('admin.entry', ["data"=>$data, 'fields'=>\App\OfficialEntryForm::$fields]);
+	$data = \App\OfficialEntryForm::all();
+	return view('admin.entry', ["data" => $data, 'fields' => \App\OfficialEntryForm::$fields]);
 });
 
 Route::get('/admin', function () {
-    $data = ContestantsInformationForm::all();
-    return view('admin.admin', ["data"=>$data]);
+	$data = ContestantsInformationForm::all();
+	return view('admin.admin', ["data" => $data]);
 });
